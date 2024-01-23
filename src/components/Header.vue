de1a05e7eac83dcd262b654988e296147a6646d6
Denis <redman1922svap@mail.ru>
2024-01-23 14:06:43 +0300

    Add layout header

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class='flex justify-between border-b border-slate-300 px-10 py-8'>
+    <div class='flex items-center gap-4'>
+      <img src='/logo.png' alt='Logo' class='w-10'>
+      <div>
+        <h2 class='text-xl font-bold uppercase'>Vue Market</h2>
+        <p class='text-slate-400'>Магазин лучших кроссовок</p>
+      </div>
+    </div>
+    <ul class='flex items-center gap-10'>
+      <li class='flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black'>
+        <img src='/cart.svg' alt='cart'>
+        <b>1205 руб.</b>
+      </li>
+      <li class='flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black'>
+        <img src='/heart.svg' alt='heart'>
+        <span>Закладки</span>
+      </li>
+      <li class='flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black'>
+        <img src='/profile.svg' alt='profile-photo'>
+        <span>Профиль</span>
+      </li>
+    </ul>
+  </header>
+</template>
