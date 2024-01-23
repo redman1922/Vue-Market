8729f1a19f385aec175350ca4a9d6bdf8d23ff3f
Denis <redman1922svap@mail.ru>
2024-01-23 13:45:50 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+  <div
+    class='bg-white  w-4/5 m-auto h-screen'
+  >
+    <h1>Denis</h1>
+  </div>
+
+</template>
+
+<style scoped>
+</style>
