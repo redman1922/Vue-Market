aeb3bce910e5c5b80222cc84d55a9a62ae47207f
Denis <redman1922svap@mail.ru>
2024-01-24 15:09:15 +0300

    Search + filters

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,29 @@ import Drawer from '@/components/Drawer.vue'
 </script>
 
 <template>
-  <Drawer />
+  <!--  <Drawer />-->
   <div
     class='bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14'
   >
     <Header />
     <div class='p-10'>
-      <h2 class='text-3xl font-bold mb-8'>Все кроссовки</h2>
+      <div class='flex justify-between items-center'>
+        <h2 class='text-3xl font-bold mb-8'>Все кроссовки</h2>
 
+        <div class='flex gap-4'>
+          <select class='py-2 px-3 border rounded-md outline-none'>
+            <option>По названию</option>
+            <option>По цене (дешево)</option>
+            <option>По цене (дорого)</option>
+          </select>
+
+          <div class='relative'>
+            <img class='absolute left-4 top-3' src='/search.svg' alt='search'>
+            <input class='border rounded-md py-1.5 pl-11 pr-4 outline-none focus:border-gray-400' type='text'
+                   placeholder='Поиск...' />
+          </div>
+        </div>
+      </div>
       <CardList />
     </div>
   </div>
